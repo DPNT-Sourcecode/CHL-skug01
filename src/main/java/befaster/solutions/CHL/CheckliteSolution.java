d5e9d6300f56566dc12434260bcdf3ee0e46d3b1
sbx_user1051 <sbx_user1051@169.254.40.233>
2019-10-17 14:56:02 +0000

    Thu Oct 17 14:56:02 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,9 +1,60 @@
 package befaster.solutions.CHL;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckliteSolution {
-    public Integer checklite(String skus) {
-        throw new SolutionNotImplementedException();
+
+	private static class Discount {
+		private Integer multiple;
+		private Integer cost;
+		public Discount(Integer multiple, Integer cost) {
+			this.multiple = multiple;
+			this.cost = cost;
+		}
+	}
+	private static class StockItem{
+		private String sku;
+		private Integer price;
+		private Discount discount;
+		public StockItem(String sku, Integer price, Discount discount) {
+			this.sku = sku;
+			this.price = price;
+			this.discount = discount;
+		}
+	}
+	private Map<String,StockItem> catalogue = new HashMap<>();
+	
+	public CheckliteSolution() {
+		catalogue.put("A", new StockItem("A", 50, new Discount( 3, 130)));
+		catalogue.put("B", new StockItem("B", 30, new Discount( 2, 45)));
+		catalogue.put("C", new StockItem("C", 20, null));
+		catalogue.put("D", new StockItem("D", 15, null));
+	}
+	
+	private Map<String,Integer> basket = new HashMap<>(); 
+	public Integer checklite(String skus) {
+        for (int i = 0; i < skus.length(); i++) {
+        	String key = skus.substring(i,i+1);
+        	if ( catalogue.containsKey(key)){
+        		if (!basket.containsKey(key)) {
+        			basket.put(key, Integer.valueOf(1));
+        		}else {
+        			basket.put(key, basket.get(key) + 1);
+        		}
+        	}
+        }
+        Integer basketTotal = Integer.valueOf(0);
+        for( Entry lineItem : basket.entrySet()) {
+        	basketTotal += calculateLineItemCost((StockItem)lineItem);
+        }
+        return basketTotal;
     }
+	private Integer calculateLineItemCost(Entry lineItem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
