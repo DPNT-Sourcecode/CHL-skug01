765af64bec3bdc5f07b3a626861de3cfd4ffd61a
sbx_user1051 <sbx_user1051@169.254.36.173>
2019-10-17 10:54:24 +0000

    Thu Oct 17 10:54:24 UTC 2019

--- src/main/java/befaster/solutions/IRNG/IntRangeSolution.java
+++ src/main/java/befaster/solutions/IRNG/IntRangeSolution.java
@@ -2,13 +2,19 @@ package befaster.solutions.IRNG;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class IntRangeSolution {
 
     public List<Integer> generate(int start_incl, int end_excl) {
-        throw new SolutionNotImplementedException();
+        List <Integer> list = new ArrayList<>();
+        for ( int i = start_incl; i < end_excl; i++) {
+        	list.add(Integer.valueOf(i));
+        }
+        return list;
     }
 
 }
+
